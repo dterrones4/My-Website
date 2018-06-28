6dc29c510094f468b3801968290ef7600f9a59b5
Daniel Terrones <Dterrones444@gmail.com>
2018-06-28 12:31:31 -0500

    addeed form submission linked to email

---
+++ main.js
@@ -0,0 +1,41 @@
+handlePageClicks();
+
+function handlePageClicks(){
+	$('#workBtn').on('click', function(event){
+		$('body').addClass('is-article-visible');
+		$('#work').removeClass('inactive');
+		$('#work').addClass('active');
+		listenForClickOutOfArticle(1);
+	})
+
+	$('#aboutMeBtn').on('click', function(event){
+		$('body').addClass('is-article-visible');
+		$('#intro').removeClass('inactive');
+		$('#intro').addClass('active');
+		listenForClickOutOfArticle(0);
+	})
+
+	$('#contactBtn').on('click', function(event){
+		$('body').addClass('is-article-visible');
+		$('#contact').removeClass('inactive');
+		$('#contact').addClass('active');
+		listenForClickOutOfArticle(2);
+	})
+}
+
+function listenForClickOutOfArticle(i){
+	let articleList = document.getElementsByClassName('active');
+	let modal = articleList[0];
+
+	let closeBtnList = document.getElementsByClassName('close');
+	let closeBtn = closeBtnList[i];
+
+	//when user clicks outside of current window close the window
+	window.onclick = function(event){
+		if(event.target == closeBtn){
+			$(modal).removeClass('active');
+			$(modal).addClass('inactive');
+			$('body').removeClass('is-article-visible')
+		}
+	}
+}
